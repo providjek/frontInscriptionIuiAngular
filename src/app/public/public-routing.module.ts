d10ba0af4f9fab760e37539fe76837be76918208
kepya <christiankepya@gmail.com>
2023-02-17 01:35:22 +0100

    update

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {PLayoutComponent} from "./p-layout/p-layout.component";
-import {RegisterComponent} from "./register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { PLayoutComponent } from "./p-layout/p-layout.component";
+import { RegisterComponent } from "./register/register.component";
 
 
 const routes: Routes = [
-      {path: '', component : PLayoutComponent, children : [
-      {path: '', redirectTo: 'home', pathMatch: 'full'},
-      {path : 'home', component : HomeComponent },
-      {path : 'register', component : RegisterComponent }
-        ]}
+  {
+    path: '', component: PLayoutComponent, children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'register', component: RegisterComponent }
+    ]
+  }
 ];
 
 
